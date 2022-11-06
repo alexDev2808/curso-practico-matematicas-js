0e234208f129b5c800898e1a5a5879a365a3d0eb
alexDev2808 <colores.2808.8000@gmail.com>
2022-11-05 19:52:04 -0600

    Agregando la opcion de utilizar cupones de descuento, pero con arrays

--- descuentos/descuentos.js
+++ descuentos/descuentos.js
@@ -7,27 +7,51 @@ const formDescuentos = document.querySelector("#formDescuentos");
 
 formDescuentos.addEventListener('submit', calcularDescuentos);
 
+const couponsList = [];
+
+couponsList.push({
+    name: "PLATZIDAY",
+    discount: 30,
+})
+couponsList.push({
+    name: "PLATZICONF",
+    discount: 35,
+})
+couponsList.push({
+    name: "PLATZIFEST",
+    discount: 20,
+})
+
 
 function calcularDescuentos(event){
     event.preventDefault();
-
-    const couponsAvailables = {
-        PLATZICONF: 30,
-        PLATZIFEST: 25,
-        PLATZIDAY: 20,
-        ALEXDEV2808: 35,
-        'PLAT21PLUS': 40
-    }
-
     const couponInput = discount.value
 
-    const applyDiscount = couponsAvailables[couponInput];
+
+    function isCouponInArray(couponElement){
+        return couponElement.name == couponInput;
+    }
+
+    // Find devuelve el objeto directamente
+    const couponInArray = couponsList.find(isCouponInArray)
+
+
+    // const couponsAvailables = {
+    //     PLATZICONF: 30,
+    //     PLATZIFEST: 25,
+    //     PLATZIDAY: 20,
+    //     ALEXDEV2808: 35,
+    //     'PLAT21PLUS': 40
+    // }
+
+
+    const applyDiscount = couponInArray.discount;
     
     if(!price.value || !discount.value){
         result.innerText = "Por favor escribe los valores correspondientes!"
         return;
     }
-    if(!applyDiscount){
+    if(!couponInArray){
         result.innerText = "El cupon que escribiste no existe!"
         return;
     }
