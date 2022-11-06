9d83699114aa6129652a726cfad0e798a572f7bb
alexDev2808 <colores.2808.8000@gmail.com>
2022-11-05 18:55:32 -0600

    Agregando la opcion de utilizar cupones de descuento

--- descuentos/descuentos.js
+++ descuentos/descuentos.js
@@ -7,17 +7,31 @@ const formDescuentos = document.querySelector("#formDescuentos");
 
 formDescuentos.addEventListener('submit', calcularDescuentos);
 
+
 function calcularDescuentos(event){
     event.preventDefault();
+
+    const couponsAvailables = {
+        PLATZICONF: 30,
+        PLATZIFEST: 25,
+        PLATZIDAY: 20,
+        ALEXDEV2808: 35,
+        'PLAT21PLUS': 40
+    }
+
+    const couponInput = discount.value
+
+    const applyDiscount = couponsAvailables[couponInput];
+    
     if(!price.value || !discount.value){
-        result.innerText = "No has escrito nada, por favor escribe los valores correspondientes!"
+        result.innerText = "Por favor escribe los valores correspondientes!"
         return;
     }
-    if(discount.value > 100){
-        result.innerText = "Ijole no se va a poder!"
+    if(!applyDiscount){
+        result.innerText = "El cupon que escribiste no existe!"
         return;
     }
-    const finalResult = (parseFloat(price.value ) * (100 - parseFloat(discount.value))) / 100;
+    const finalResult = (parseFloat(price.value ) * (100 - parseFloat(applyDiscount))) / 100;
     result.innerText = "Precio con Descuento: " + finalResult.toFixed(2);
 
 }
